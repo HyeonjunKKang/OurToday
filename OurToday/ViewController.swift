7d5671d61944b5e684be9b042c392faca7e239e3
HyeonJun Kang <khjj9865@naver.com>
2023-04-15 16:51:44 +0900

    Initial Commit

---
+++ OurToday/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OurToday
+//
+//  Created by 강현준 on 2023/04/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
