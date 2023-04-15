a85ed232c72e1c47346a2016006eb3f02b9bb1d2
HyeonJun Kang <khjj9865@naver.com>
2023-04-15 22:24:31 +0900

    initial commit

--- OurToday/Controller/MainViewController.swift
+++ OurToday/Controller/MainViewController.swift
@@ -6,15 +6,50 @@
 //
 
 import UIKit
+import SnapKit
 
 class MainViewController: UIViewController{
     
-    // MARK: - Lifecycle
+    // MARK: - Properties
+    
+    let logoImage = #imageLiteral(resourceName: "mainLogo")
+    let mainView = MainView()
+    
+    
+    
+    // MARK: - LifeCycle
+    
+    override func loadView() {
+        self.view = mainView
+    }
     
     override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureViewController()
+        configureUI()
+    }
+    
+    // MARK: - Actions
+    
+    @objc func didTapSetting(){
         
     }
     
+    // MARK: - Helper
     
+    func configureViewController(){
+        
+        navigationItem.titleView = UIImageView(image: logoImage)
+
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "Gear").resize(to: CGSize(width: 24, height: 24)), style: .done, target: self, action: #selector(didTapSetting))
+        
+    }
     
+    func configureUI(){
+        
+        navigationController?.navigationBar.isTranslucent = false
+        
+
+    }
 }
