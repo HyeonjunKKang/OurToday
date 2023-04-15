6c1950d87cbeffece6d0c11fd57f9ff3f025860c
HyeonJun Kang <khjj9865@naver.com>
2023-04-15 17:38:00 +0900

    initial commit

--- OurToday/Controller/MainTabController.swift
+++ OurToday/Controller/MainTabController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainTabController.swift
 //  OurToday
 //
 //  Created by 강현준 on 2023/04/15.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MainTabController: UITabBarController{
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
-
---
+++ OurToday/Controller/MainViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MainViewController.swift
+//  OurToday
+//
+//  Created by 강현준 on 2023/04/15.
+//
+
+import UIKit
+
+class MainViewController: UIViewController{
+    override func viewDidLoad() {
+        
+    }
+}
