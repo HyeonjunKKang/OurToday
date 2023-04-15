c6ad390bdb2e05981701aaaa333960bff740f99b
HyeonJun Kang <khjj9865@naver.com>
2023-04-15 18:29:11 +0900

    [Feat] Add Inset, resize

---
+++ OurToday/Utils/UIImage+Extension.swift
@@ -0,0 +1,30 @@
+//
+//  UIImage+Extension.swift
+//  OurToday
+//
+//  Created by 강현준 on 2023/04/15.
+//
+
+import UIKit
+
+extension UIImage{
+    func imageWithInsets(insets: UIEdgeInsets) -> UIImage{
+        UIGraphicsBeginImageContextWithOptions(
+            CGSize(width: self.size.width + insets.left + insets.right,
+                   height: self.size.height + insets.top + insets.bottom), false, self.scale)
+        let _ = UIGraphicsGetCurrentContext()
+        let origin = CGPoint(x: insets.left, y: insets.top)
+        self.draw(at: origin)
+        let imageWithInsets = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return imageWithInsets ?? self
+    }
+    
+    func resize(to size: CGSize) -> UIImage? {
+            UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+            defer { UIGraphicsEndImageContext() }
+            draw(in: CGRect(origin: .zero, size: size))
+            return UIGraphicsGetImageFromCurrentImageContext()?.withRenderingMode(renderingMode)
+    }
+}
+
