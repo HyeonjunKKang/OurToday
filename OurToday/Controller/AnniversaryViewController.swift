9d9379285d269477918580f4b9064a83ce423265
HyeonJun Kang <khjj9865@naver.com>
2023-04-16 16:52:18 +0900

    feat: final 키워드 추가

--- OurToday/Controller/AnniversaryViewController.swift
+++ OurToday/Controller/AnniversaryViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class AnniversaryViewController: UIViewController {
+final class AnniversaryViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
