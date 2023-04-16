96e5046c550d2ea13daa8e0f6d07d92d4cf983d8
HyeonJun Kang <khjj9865@naver.com>
2023-04-16 16:49:30 +0900

    feat: add ImagePickerDelegate

---
+++ OurToday/Api/DelegateProtocol.swift
@@ -0,0 +1,17 @@
+//
+//  PickTheDayAndBirthDayDelegate.swift
+//  OurToday
+//
+//  Created by 강현준 on 2023/04/16.
+//
+
+import UIKit
+
+protocol PickDateDelegate: AnyObject{
+    func dayDidComplete(controller: UIViewController)
+    func pickDay(controller: UIViewController, selecteDay: Date)
+}
+
+protocol ImagePickerDelegate: AnyObject{
+    func imagePick(_ controller: UIViewController, image: UIImage)
+}
