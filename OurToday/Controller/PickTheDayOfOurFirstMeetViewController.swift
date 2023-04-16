b0543e12f88888a45ea86975897cf7cad75bc4fa
HyeonJun Kang <khjj9865@naver.com>
2023-04-16 16:55:23 +0900

    Feat: 첫 만남날 선택 기능

---
+++ OurToday/Controller/PickTheDayOfOurFirstMeetViewController.swift
@@ -0,0 +1,53 @@
+//
+//  PickTheDayOfOurFirstMeetViewController.swift
+//  OurToday
+//
+//  Created by 강현준 on 2023/04/15.
+//
+
+import UIKit
+import SnapKit
+import FSCalendar
+
+final class PickTheDayOfOurFirstMeetViewController: UIViewController{
+    
+    // MARK: - Properties
+    
+    weak var delegate: PickDateDelegate?
+    
+    let pickTheDayView = IncludeCalenderView(text: "언제부터 사랑하셨나요?")
+    
+    // MARK: - LifeCycle
+    
+    override func loadView() {
+        self.view = pickTheDayView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configure()
+    }
+    
+    // MARK: - Action
+    
+    @objc func checkNext(){
+        delegate?.dayDidComplete(controller: self)
+    }
+    
+    // MARK: - Helper
+    
+    func configure(){
+        pickTheDayView.fsCalendar.delegate = self
+        pickTheDayView.nextButton.addTarget(self, action: #selector(checkNext), for: .touchUpInside)
+    }
+}
+
+// MARK: - FSCalendarDelegate
+
+extension PickTheDayOfOurFirstMeetViewController: FSCalendarDelegate{
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        let selectedDate = date
+        delegate?.pickDay(controller: self, selecteDay: selectedDate)
+    }
+}
