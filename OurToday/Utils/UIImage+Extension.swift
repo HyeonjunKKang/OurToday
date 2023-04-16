7e2e46202f02f105702dca280c70c86e98d913f6
HyeonJun Kang <khjj9865@naver.com>
2023-04-16 16:56:55 +0900

    Feat: resize메서드 추가

--- OurToday/Utils/UIImage+Extension.swift
+++ OurToday/Utils/UIImage+Extension.swift
@@ -21,10 +21,10 @@ extension UIImage{
     }
     
     func resize(to size: CGSize) -> UIImage? {
-            UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
-            defer { UIGraphicsEndImageContext() }
-            draw(in: CGRect(origin: .zero, size: size))
-            return UIGraphicsGetImageFromCurrentImageContext()?.withRenderingMode(renderingMode)
+        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+        defer { UIGraphicsEndImageContext() }
+        draw(in: CGRect(origin: .zero, size: size))
+        return UIGraphicsGetImageFromCurrentImageContext()?.withRenderingMode(renderingMode)
     }
 }
 
