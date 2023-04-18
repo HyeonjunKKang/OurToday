4996680fcb0ffc4236570762d87e9b140f951ac6
HyeonJun Kang <khjj9865@naver.com>
2023-04-18 15:33:37 +0900

    Feat: 파이어베이스 추가

--- OurToday/Utils/Constant.swift
+++ OurToday/Utils/Constant.swift
@@ -6,7 +6,16 @@
 //
 
 import UIKit
+import Firebase
 
-let PINKCOLOR = UIColor(red: 253/255, green: 142/255, blue: 142/255, alpha: 1)
+let PINKCOLOR = UIColor(red: 253, green: 142, blue: 142)
+
+let GRAYTEXTCOLOR = UIColor(red: 144, green: 137, blue: 137)
 
 let USERDEFAULT = "LoveModel"
+
+let CELL_RECOMMEND = "RecommendDateCell"
+
+let COLLECTION_RECOMMEND = Firestore.firestore().collection("recommenddate")
+
+let ERROR_IMAGE = #imageLiteral(resourceName: "에러이미지")
