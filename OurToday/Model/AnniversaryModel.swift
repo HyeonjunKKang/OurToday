8e4b68f486c1e1aa766473d15f434a38197f9faa
HyeonJun Kang <khjj9865@naver.com>
2023-04-18 17:28:06 +0900

    inital commit

---
+++ OurToday/Model/AnniversaryModel.swift
@@ -0,0 +1,14 @@
+//
+//  AnniversaryModel.swift
+//  OurToday
+//
+//  Created by 강현준 on 2023/04/18.
+//
+
+import Foundation
+
+struct AnniversaryModel{
+    var whatDay: String = "0일"
+    var date: String = "0000.00.00(월)"
+    var remainDay: String = "D-day 9"
+}
