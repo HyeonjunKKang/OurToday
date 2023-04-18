34560ea81f9e4a8e06d132bc905f3053b6c5237d
HyeonJun Kang <khjj9865@naver.com>
2023-04-18 17:28:45 +0900

    Fix: 유저디폴트 Realm 변경

--- OurToday/Api/UserDefaultsManager.swift
+++ OurToday/Api/UserDefaultsManager.swift
@@ -4,26 +4,3 @@
 //
 //  Created by 강현준 on 2023/04/16.
 //
-
-import UIKit
-
-final class UserDefaultsManager{
-    static var shared = UserDefaultsManager()
-    
-    
-    func loadUserDefaults(completion: (LoveModel) -> ()){
-        guard let data = UserDefaults.standard.value(forKey: USERDEFAULT) as? Data,
-              let LoveData = try? PropertyListDecoder().decode(LoveModel.self, from: data) else {
-            
-            writeUserDefaults(loveModel: LoveModel())
-            completion(LoveModel())
-            return
-        }
-        completion(LoveData)
-    }
-    
-    func writeUserDefaults(loveModel: (LoveModel)){
-        UserDefaults.standard.set(try? PropertyListEncoder().encode(loveModel), forKey: USERDEFAULT)
-    }
-    
-}
