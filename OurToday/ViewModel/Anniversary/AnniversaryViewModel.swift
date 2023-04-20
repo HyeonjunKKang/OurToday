41419f300d569e7ed76ce8a0875129bbfca0f10a
HyeonJun Kang <khjj9865@naver.com>
2023-04-20 11:25:16 +0900

    Refactor: Service 분리

---
+++ OurToday/ViewModel/Anniversary/AnniversaryViewModel.swift
@@ -0,0 +1,54 @@
+//
+//  AnniversaryViewModel.swift
+//  OurToday
+//
+//  Created by 강현준 on 2023/04/18.
+//
+
+import UIKit
+
+final class AnniversaryViewModel{
+    
+    // MARK: - Properties
+    
+    private var loveModel: LoveModel{
+        didSet{
+            calculateAnniversary()
+        }
+    }
+    
+    private var anniversaryList: [AnniversaryModel] = []
+    
+    let service = AnniversaryService.shared
+    
+    var loveModelDidChage : (() -> ())?
+    
+    var anniversaryListCount: Int{
+        return anniversaryList.count
+    }
+    
+    // MARK: - Initializer
+    
+    init(loveModel: LoveModel) {
+        self.loveModel = loveModel
+        calculateAnniversary()
+    }
+    
+    // MARK: - Helper
+    
+    func calculateAnniversary(){
+        self.service.calculateAnniversary(loveModel: loveModel) { [weak self] anniversaryList in
+            self?.anniversaryList = anniversaryList
+            self?.loveModelDidChage?()
+            dump(self?.anniversaryList)
+        }
+    }
+    
+    func getOnlyanniversaryList(_ index: Int) -> AnniversaryModel{
+        return anniversaryList[index]
+    }
+    
+    func setLoveModel(loveModel: LoveModel){
+        self.loveModel = loveModel
+    }
+}
