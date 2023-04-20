696c446c1d675b7245b1ee5c170ef1464c2f5fab
HyeonJun Kang <khjj9865@naver.com>
2023-04-20 11:27:16 +0900

    Refactor: 불필요한 이미지 로드 메서드 삭제

--- OurToday/ViewModel/RecommendDateViewModel.swift
+++ OurToday/ViewModel/RecommendDateViewModel.swift
@@ -29,26 +29,6 @@ struct RecommendDateViewModel{
         return date.name
     }
     
-    func loadImage(url string: String, completion: @escaping(UIImage) -> ()){
-        if let url = URL(string: string){
-            let task = URLSession.shared.dataTask(with: url) { data, response, error in
-               
-                if let error = error{
-                    print("DEBUG: Error downloading image \(error.localizedDescription)")
-                    
-                    return
-                }
-                
-                guard let data = data, let image = UIImage(data: data) else {
-                    print("DEBUG: Error converting data to image")
-                    
-                    return
-                }
-                completion(image)
-            }.resume()
-        }
-    }
-    
     // MARK: - Initializer
     
     init(date: DateModel) {
