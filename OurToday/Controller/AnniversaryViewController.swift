75f3e0cad5e35c34cb1a419cd4d443733d1be3d3
HyeonJun Kang <khjj9865@naver.com>
2023-04-24 11:50:08 +0900

    Feat: 디데이 확인 기능 추가

--- OurToday/Controller/AnniversaryViewController.swift
+++ OurToday/Controller/AnniversaryViewController.swift
@@ -6,24 +6,79 @@
 //
 
 import UIKit
+import SnapKit
 
-final class AnniversaryViewController: UIViewController {
+final class AnniversaryViewController: UICollectionViewController {
+    
+    // MARK: - Properties
+    
+    private var viewModel: AnniversaryViewModel
+    
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configureCollectionView()
+        showLoader(true)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Initializer
+    
+    init(viewModel: AnniversaryViewModel, collectionviewLayout: UICollectionViewFlowLayout) {
+        self.viewModel = viewModel
+        super.init(collectionViewLayout: collectionviewLayout)
+        setViewModelLoveModelDidChange()
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Helper
+    
+    func configureCollectionView(){
+        navigationItem.titleView = UIImageView(image: #imageLiteral(resourceName: "mainLogo"))
+        collectionView.register(AnniversaryCell.self, forCellWithReuseIdentifier: CELL_ANNIVERSARY)
+        collectionView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    func setViewModelLoveModelDidChange(){
+        // 델리게이트 패턴이 아닌 클로저형식의 값, 이벤트 처리
+        viewModel.loveModelDidChage = { [weak self] in
+            guard let self = self else { return }
+            
+            DispatchQueue.main.async {
+                self.collectionView.reloadData()
+                self.showLoader(false)
+            }
+        }
+    }
+    
+    func bind(lovedata: LoveModel){
+        self.viewModel.setLoveModel(loveModel: lovedata)
+    }
+}
 
+// MARK: - UICollectionViewDataSource
+
+extension AnniversaryViewController{
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CELL_ANNIVERSARY, for: indexPath) as? AnniversaryCell else { return UICollectionViewCell()}
+        cell.viewModel = AnniversaryCellViewModel(anniversaryModel: viewModel.getOnlyanniversaryList(indexPath.row))
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.anniversaryListCount
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+
+extension AnniversaryViewController: UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: 70)
+    }
 }
